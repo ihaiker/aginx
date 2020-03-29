635ab06907c763800f90770024a7b5c3ef5fa391
haiker <wo@renzhen.la>
2020-03-29 16:25:39 +0800

    fixbug: set default command

--- conf/root_command.go
+++ conf/root_command.go
@@ -10,9 +10,10 @@ func SetDefaultCommand(root, setDef *cobra.Command) {
 	//set node is default command
 	if runCommand, args, err := root.Find(os.Args[1:]); err == nil {
 		if runCommand == root {
-			runCommand.InitDefaultHelpFlag()
-			if help, err := runCommand.PersistentFlags().GetBool("help"); err == nil && help {
-				//show help
+			root.SetArgs(args)
+			root.InitDefaultHelpFlag()
+			if help, err := root.Flags().GetBool("help"); err == nil && help {
+				// show help
 			} else {
 				idx := 1
 				for _, arg := range args {
@@ -29,13 +30,16 @@ func SetDefaultCommand(root, setDef *cobra.Command) {
 								idx += 2
 							}
 							continue
-						} else if f = root.PersistentFlags().ShorthandLookup(flagName); f != nil {
-							if f.Value.Type() == "bool" || hasValue > 0 {
-								idx += 1
-							} else if f.Value.String() != "" {
-								idx += 2
+						}
+						if len(flagName) == 1 {
+							if f := root.PersistentFlags().ShorthandLookup(flagName); f != nil {
+								if f.Value.Type() == "bool" || hasValue > 0 {
+									idx += 1
+								} else if f.Value.String() != "" {
+									idx += 2
+								}
+								continue
 							}
-							continue
 						}
 					}
 					break
